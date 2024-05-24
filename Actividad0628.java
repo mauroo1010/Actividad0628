95723fab7f71eded147b2615c69479a66c043e31
mauroo1010 <maurogutierreztormo@gmail.com>
2024-05-24 13:04:38 +0200

    Pasos iniciales

---
+++ Actividad0628.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Actividad0628 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Actividad0628
+{
+    public int jugandoALosDardos(double x, double y) {
+        int z = 1;
+        return z;
+    }
+}
+
